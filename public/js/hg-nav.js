dd00b8bf4588ec20c731f67d78234249d2ba9197
Thiago Sciotta <thiagog3@gmail.com>
2016-03-19 12:39:55 -0300

    Starting load menu functions...

--- public/js/hg-nav.js
+++ public/js/hg-nav.js
@@ -10,16 +10,34 @@
 
     if (arguments[0] && typeof arguments[0] === "object") {
       this.options = extendDefaults(defaults, arguments[0]);
+    }else{
+      this.options = defaults;
     }
+
+    loadMenuFromJson(this.options.jsonDataUrl);
   };
 
+  function loadMenuFromJson(jsonUrl){
+    var req = new XMLHttpRequest();
+    req.onload = function (e) {
+      buildMenu(e.target.response);
+    };
+    req.open('GET', jsonUrl, true);
+    req.responseType = 'json';
+    req.send();
+  }
+
+  function buildMenu(menuObj){
+    console.log(menuObj);
+  }
+
   function initializeMenuEvent(){
-    
+
   }
 
   function extendDefaults(source, properties) {
     var property;
-    for (property in properties) {
+    for (property in poperties) {
       if (properties.hasOwnProperty(property)) {
         source[property] = properties[property];
       }
